fee6b8c877bf4fde9a28f4b612fbec091436cfeb
travisfriesen <github@travisfriesen.ca>
2025-01-25 09:57:09 -0600

    added: install script to install reveal.js

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.13.1".
+nvm current # Should print "v22.13.1".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
+
+sudo apt install git -y
+
+mkdir slides && cd slides
+git clone https://github.com/hakimel/reveal.js.git
+cd reveal.js && npm install
