7caa37a77fcc89c5e19527d93f0069561cbd4603
travisfriesen <github@travisfriesen.ca>
2025-01-25 15:05:02 -0600

    added: samba share init

--- install_samba.sh
+++ install_samba.sh
@@ -3,3 +3,8 @@
 adduser --system --no-create-home slides
 
 echo "Enter a password for the share access account." && passwd slides
+
+mkdir /images
+cp -r images /images
+
+cp smb.conf /etc/samba/smb.conf
