fbeb5c785f797f93e8fe403de1378a29aa163294
travisfriesen <github@travisfriesen.ca>
2025-01-25 19:55:41 -0600

    added: make file rebuilding

--- generate_slides.sh
+++ generate_slides.sh
@@ -2,9 +2,14 @@
 
 sudo apt install python3 #just check that python is installed
 
+make clean #clean old file
+
 rm -rf slides && mkdir slides
 cp -r images/* slides
 
 python3 ./generate_slides.py
 
 #run slides
+make
+
+DISPLAY=:0. chromium-browser slides.html --incognito --kiosk
